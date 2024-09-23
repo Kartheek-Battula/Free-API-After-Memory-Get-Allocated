7f615ed54f344dc3d900b2a682bbd32ef7b18453
Kartheek-Battula <karthikbattula2000@gmail.com>
2024-09-23 14:57:22 +0530

    Update assg.c

--- assg2.c
+++ assg2.c
@@ -1,9 +1,12 @@
 /*
-Name :
-Date :
-Description :
-Sample Input :
-Sample Output :
+    You have a Heap of Size 128KB. Minimum Block Size to be given out is 1 KB. Design a Malloc and Free API to give and take memory back into the heap. Keep the following constraints :
+->Return Pointer to the memory
+->No memory can be given out twice consecutively
+->No memory can be freed Twice consecutively
+->Find the Best Fit for the memory SIze that is asked to reduce the memory wastage.
+Example Calls :
+malloc( sizeof(int) * 128 ) ; // Should give a 1KB Block
+free(ptr);
 */
 #include <stdio.h>
 #include <stdbool.h>
